89607cb7ddddcbdb25e2a8c77332e25bd638bc38
nandhuchennamsetty <130283853+nandhuchennamsetty@users.noreply.github.com>
2024-09-25 16:36:36 +0530

    Update server.js
    updating cors

--- Backend/server.js
+++ Backend/server.js
@@ -16,9 +16,9 @@ mongoose
     )
 
 PORT = process.env.PORT || 5000
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 app.use('/api',router)
 app.listen(PORT, () => {
     console.log(`app is Listening at ${PORT} port`);
